186c3af88f75fd7bd4335ee3cc2cc4cf4e00cffa
Sebs-rss <seba.r.sanchez@gmail.com>
2024-10-14 12:48:23 -0300

    segundo commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,13 @@
+// Módulo 5, día 8, desafío evaluado: "traer posts"
+// By Sebastián Sánchez
+// 1) Realizar una consulta asyncrona (request) a la API usando async-awai
+
+
+
+
+//2) Mostrar el resultado del request en HTML (mostrando los post como una lista desornedada)
+
+
+
+
+//3) Manejo de errores con try-catch
